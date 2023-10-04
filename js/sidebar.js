4397e4bd8c598f1c2a0b1ce64a008c8885f1f5f6
Cade Meu Code <cademeucode@iMac-de-Abudu.local>
2023-10-04 22:24:04 +0900

    change to dark theme

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,15 +1,18 @@
-if(window.innerWidth > 768.0001){
-    function openLink(evt, animName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("bar__content");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" magenta", "");
-    }
-    document.getElementById(animName).style.display = "block";
-    evt.currentTarget.className += " magenta";
-  }
+if (window.innerWidth > 768.0001) {
+	function openLink(evt, animName) {
+
+		let x = document.getElementsByClassName("bar__content");
+		for (i = 0; i < x.length; i++) {
+			x[i].style.display = "none";
+		}
+
+		let tablinks = document.getElementsByClassName("tablink");
+		for (i = 0; i < x.length; i++) {
+			tablinks[i].className = tablinks[i].className.replace(" magenta", "");
+		}
+
+		document.getElementById(animName).style.display = "block";
+		evt.currentTarget.className += " magenta";
+
+	}
 }
